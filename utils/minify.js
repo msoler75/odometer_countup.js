6f2bd62f6190a32d243fd522bf1795ac94755fd7
inorganik <jamie.perkins@gmail.com>
2025-04-24 05:57:28 -0600

    remove old build deps

--- utils/minify.js
+++
@@ -1,6 +0,0 @@
-const fs = require('fs');
-const uglify = require('uglify-js');
-
-const result = uglify.minify(fs.readFileSync('src/odometer.js', 'utf8'));
-
-fs.writeFileSync('dist/odometer.min.js', result.code);
